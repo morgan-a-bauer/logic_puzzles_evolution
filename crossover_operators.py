cbd39ff09b616ebc89b7e5627219c0edd3200849
Morgan Bauer <morgan.a.bauer@gmail.com>
2024-04-26 14:31:50 -0400

    Cycle crossover done

--- crossover_operators.py
+++ crossover_operators.py
@@ -7,20 +7,82 @@ Note: these operators will be applied elementwise to a vector of
 permutations
 
 """
+from random import randint
 
-def cycle(parents: list):
-    """Cycle crossover"""
-    parent_index = 0
-    used_numbers = set()
-    for pos, start_entry in enumerate(parents[parent_index]):
-        if start_entry not in used_numbers:
-            cycle = set()
-            cycle.append(start_entry)
+def cycle(p1: list, p2: list):
+    """An implementation of cycle crossover for evolutionary algorithms using
+    permutation representation. Finds all cycles in the permutation and swaps
+    the elements of approximately half of those cycles. Returns two lists,
+    representing the two child permutations.
 
-            parent_index += 1
-            parent_index % 2
+    Input:
+    p1 -- a permutation from the genotype of the first parent
+    p2 -- a permutation from the genotype of the second parent
 
-            new_entry = 'l'
-            while new_entry != start_entry:
-                new_entry = parents[parent_index][pos]
-                print("changes")
+    """
+    # Which elements are already identified as elements of cycles
+    used_nums = set()
+
+    # Which elements are in cycles that will be switched
+    to_switch = set()
+
+    # Count the number of cycles
+    cycle_num = 0
+
+    # Until all cycles have been identified
+    while len(used_nums) != len(p1):
+        i = 0
+
+        # If the element in p1 at index i is already in an identified cycle
+        while p1[i] in used_nums:
+            i += 1
+
+        # Identify first two elements of the cycle
+        start_num = p1[i]
+        next_num = p2[i]
+
+        # Initialize current cycle
+        cycle = {start_num, next_num}
+        used_nums.add(start_num)
+        used_nums.add(next_num)
+
+        # Continue until the starting element is reached again
+        while next_num != start_num:
+
+            # Find the next element in the cycle
+            i = p1.index(next_num)
+            next_num = p2[i]
+
+            # Update sets
+            cycle.add(next_num)
+            used_nums.add(next_num)
+
+        # Even numbered cycles will be switched
+        if cycle_num % 2 == 0:
+            to_switch |= cycle
+
+        # Increment cycle number
+        cycle_num += 1
+
+    # Initialize child permutations
+    c1_part = [0 for i in p1]
+    c2_part = [0 for i in p1]
+
+    # Populate chuld permutations
+    for i in range(len(p1)):
+
+        # Switch applicable elements
+        if p1[i] in to_switch:
+            c1_part[i], c2_part[i] = p2[i], p1[i]
+
+        else:
+            c1_part[i], c2_part[i] = p1[i], p2[i]
+
+    return c1_part, c2_part
+
+
+def pmx(p1: list, p2: list):
+    point_1, point_2 = randint(len(p1)), randint(len(p1))
+
+# Contains all crossover operators
+CROSSOVER_LYST = [cycle]
--- individual.py
+++ individual.py
@@ -6,13 +6,16 @@ A class representing an individual for an evolutionary approach to solving
 logic grid puzzles. Each individual is a vector of permutations.
 
 """
+from crossover_operators import CROSSOVER_LYST
 from random import randrange
 from util import random_perm
 
 class Individual:
-    def __init__(self, p1: list, p2: list, p3: list):
-        self.__genotype = [p1, p2, p3, [1, 2, 3, 4, 5]]
+    def __init__(self, perms: list, c_ind: int, m_ind: int):
+        self.__genotype = perms
         self.__fitness = 0.0
+        self.__crossover_index = c_ind
+        self.__mutation_index = m_ind
 
 
     @property
@@ -26,11 +29,26 @@ class Individual:
 
 
     def __mul__(self, other):
-        #TODO
-        pass
+        """Overloads the multiplication operator for recombination."""
+
+        # Determine which crossover operator is being used
+        operator = CROSSOVER_LYST[self.__crossover_index]
+
+        # Initialize child genotypes
+        c1 = []
+        c2 = []
+
+        # Populate child genotypes
+        for p1_perm, p2_perm in zip(self.__genotype, other.genotype):
+            c1_perm, c2_perm = operator(p1_perm, p2_perm)
+            c1.append(c1_perm)
+            c2.append(c2_perm)
+
+        # Instantiate and return new Individual objects representing the children
+        return Individual(c1, self.__crossover_index, self.__mutation_index),\
+               Individual(c2, self.__crossover_index, self.__mutation_index)
 
     def __str__(self) -> str:
-        print(self.__genotype)
         return "   ".join(["".join(map(str, perm)) for perm in self.__genotype])
 
 if __name__ == "__main__":
@@ -38,5 +56,16 @@ if __name__ == "__main__":
     p2 = random_perm(5)
     p3 = random_perm(5)
 
-    i1 = Individual(p1, p2, p3)
+    i1 = Individual([p1, p2, p3, [1, 2, 3, 4, 5]], 0, 0)
     print(i1)
+
+    p4 = random_perm(5)
+    p5 = random_perm(5)
+    p6 = random_perm(5)
+
+    i2 = Individual([p4, p5, p6, [1, 2, 3, 4, 5]], 0, 0)
+    print(i2)
+
+    c1, c2 = i1 * i2
+    print(c1)
+    print(c2)
