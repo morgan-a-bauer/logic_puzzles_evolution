cbf7df3b61f979465971e626e547865751a12dff
areflour <136472461+areflour@users.noreply.github.com>
2024-04-26 14:31:03 -0400

    created swap mutator that probably works

--- mutation_operators.py
+++ mutation_operators.py
@@ -8,13 +8,31 @@ permutations
 
 """
 import random
-def insert(lyst):
-    k = lyst[random.randint(0,len(lyst))] #which element will be moved
-    i = random.randint(0,len(lyst)) #where will it be moved to
+def randIndex(lyst:list):
+    return random.randint(0,len(lyst))
+def insert(lyst:list):
+    if not type(lyst) is list:
+        raise Exception("Error: input for insert was not a list")
+    k = lyst[randIndex(lyst)] #which element will be moved
+    i = randIndex(lyst) #where will it be moved to
     lyst.remove(k)
     lyst.insert(i,k)
-    return lyst
-def inverse(lyst):
+    return lyst #likely unecessary; should do it in place
+def inverse(lyst:list):
+    if not type(lyst) is list:
+        raise Exception("Error: input for inverse was not a list")
     #define bounds for the subset
-    i1 = random.randint(0,len(lyst)) #index 1
-    i2 = random.randint(0,len(lyst)) #index 2
+    i1,i2 = randIndex(lyst), randIndex(lyst)#index 1 and 2
+    pass
+    #ill do this later
+#takes in a lyst, swaps two elements at random, modifies it in place(?) as well as returns it
+def swap(lyst:list):
+    if not type(lyst) is list:
+        raise Exception("Error: input for swap was not a list")
+    i1,i2 = randIndex(lyst), randIndex(lyst)#index 1 and 2
+    temp = lyst[i1]
+    lyst[i1]=lyst[i2]
+    lyst[i2] = temp
+    return lyst #likely unecessary; should do it in place
+    
+    
