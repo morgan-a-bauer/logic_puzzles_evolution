1bf06ead4904eebde025e3497995e2c9b0d00ce6
Morgan Bauer <morgan.a.bauer@gmail.com>
2024-05-03 15:40:03 -0400

    Added to class Population

--- util.py
+++ util.py
@@ -7,7 +7,13 @@ Utility functions for an evolutionary approach to solving logic grid puzzles
 """
 from random import shuffle
 
+
 def random_perm(n: int) -> str:
     p = [i for i in range(1, n+ 1)]
     shuffle(p)
     return p
+
+
+def get_fitness(individual):
+    rules = individual.pop.rules
+    #TODO
