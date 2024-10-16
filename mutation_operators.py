994e21d3d5a8f0074b190ebf96d3a6065623ca82
Morgan Bauer <131829467+morgan-a-bauer@users.noreply.github.com>
2024-10-16 13:41:27 -0600

    Update mutation_operators.py

--- mutation_operators.py
+++ mutation_operators.py
@@ -10,25 +10,43 @@ permutations
 import random
 #TODO: Test in-place
 
-def randIndex(lyst:list):
+def randIndex(lyst: list):
+    """Select a random index from a list
+    
+    Input:
+    lyst -- the list to select an index from
+    
+    """
     return random.randint(0,len(lyst)-1)
   
 
-def insert(lyst:list):
+def insert(lyst: list):
+    """Insertion mutation
+    
+    Input:
+    lyst -- the individual to mutate
+    
+    """
     if not type(lyst) is list:
         raise Exception("Error: input for insert was not a list")
-    k = lyst[randIndex(lyst)] #which element will be moved
-    i = randIndex(lyst) #where will it be moved to
+    k = lyst[randIndex(lyst)]  # which element will be moved
+    i = randIndex(lyst)  # where will it be moved to
     lyst.remove(k)
     lyst.insert(i,k)
-    return lyst #likely unecessary; should do it in place
+    return lyst  # likely unecessary; should do it in place
 
 
 def inverse(lyst:list):
+    """Inverse mutation
+    
+    Input:
+    lyst -- the individual to mutate
+    
+    """
     if not type(lyst) is list:
         raise Exception("Error: input for inverse was not a list")
-    #define bounds for the subset
-    i1,i2 = randIndex(lyst), randIndex(lyst)#index 1 and 2
+    # define bounds for the subset
+    i1,i2 = randIndex(lyst), randIndex(lyst)  # index 1 and 2
     if i1>i2:
         i1,i2 = i2,i1
     elif i1==i2:
@@ -45,18 +63,30 @@ def inverse(lyst:list):
     return lyst2
 
 def swap(lyst:list):
+    """Swap mutation
+    
+    Input:
+    lyst -- the individual to mutate
+    
+    """
     if not type(lyst) is list:
         raise Exception("Error: input for swap was not a list")
-    #grab two random indices
+    # grab two random indices
     i1,i2 = randIndex(lyst), randIndex(lyst)
-    #swap  
+    # swap  
     lyst[i1], lyst[i2] = lyst[i2], lyst[i1]
-    return lyst #likely unecessary; should do it in place
+    return lyst  # likely unecessary; should do it in place
 
-def scramble(lyst:list): #like an egg before you get folded like an omlet
+def scramble(lyst:list):  # like an egg before you get folded like an omlette
+    """Scramble mutation
+    
+    Input:
+    lyst -- the individual to mutate
+    
+    """
     if not type(lyst) is list:
         raise Exception("Error: input for swap was not a list")
-    #this is a really complex program
+    # this is a really complex program
     random.shuffle(lyst)
     return lyst
 
